40ced5f7e0e0394dd9c5f245ece6088c8ab2e5bd
Sihao Chen <cs1819@ic.ac.uk>
2020-12-11 00:56:50 +0800

    update eval

--- eval.py
+++ eval.py
@@ -8,25 +8,28 @@ from dice_loss import dice_coeff
 def eval_net(net, loader, device):
     """Evaluation without the densecrf with the dice coefficient"""
     net.eval()
-    mask_type = torch.float32 if net.n_classes == 1 else torch.long
+    target_type = torch.float32 if net.n_classes == 1 else torch.long
     n_val = len(loader)  # the number of batch
     tot = 0
 
     with tqdm(total=n_val, desc='Validation round', unit='batch', leave=False) as pbar:
         for batch in loader:
-            imgs, true_masks = batch['image'], batch['mask']
+            imgs, true_targets = batch['image'], batch['target']
             imgs = imgs.to(device=device, dtype=torch.float32)
-            true_masks = true_masks.to(device=device, dtype=mask_type)
+            true_targets = true_targets.to(device=device, dtype=target_type)
 
             with torch.no_grad():
-                mask_pred = net(imgs)
+                target_pred = net(imgs)
 
+            tot += nn.L1Loss(target_pred, true_targets).item()
+            """
             if net.n_classes > 1:
-                tot += F.cross_entropy(mask_pred, true_masks).item()
+                tot += nn.cross_entropy(target_pred, true_targets).item()
             else:
-                pred = torch.sigmoid(mask_pred)
+                pred = torch.sigmoid(target_pred)
                 pred = (pred > 0.5).float()
-                tot += dice_coeff(pred, true_masks).item()
+                tot += dice_coeff(pred, true_targets).item()
+            """
             pbar.update()
 
     net.train()
